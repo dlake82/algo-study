90a0173e63829c4e5cca9d0fbbcc293598fba4db
cskhw <51100329+cskhw@users.noreply.github.com>
2023-01-29 02:13:05 +0900

    feat: 연속된 수의 합

---
+++ programmers/2-back big num find.py
@@ -0,0 +1,24 @@
+def solution(num, total):
+    answer = []
+
+    if total == 0:
+        for i in range(num):
+            num // 2
+    
+    for i in range(total, 0, -1):
+        print(i)
+        SUM = 0
+        for j in range(num):
+            now = i - j
+            SUM += now
+        print(SUM, total)
+            
+        if SUM == total:
+            for k in range(num):
+                answer.append(i - num + k + 1)
+            return answer
+    
+    return answer
+
+ret = solution(3, 0)
+print(ret)
