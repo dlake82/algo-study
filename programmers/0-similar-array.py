1f3871e84bde77e37769d3dd152f5ea339bdeea8
cskhw <51100329+cskhw@users.noreply.github.com>
2023-03-30 11:05:06 +0900

    feat: 알고 추가

--- programmers/0-similar-array.py
+++ programmers/0-similar-array.py
@@ -1,19 +1,18 @@
 from collections import defaultdict
 
+
 def solution(s):
-    answer = ''
+    answer = ""
 
     d = defaultdict(int)
     for key in list(s):
         if not d[key]:
             d[key] = 0
-        
+
         d[key] += 1
-    
+
     for k, v in d.items():
         if v == 1:
             answer += k
-    
-
 
     return "".join(sorted(list(answer)))
