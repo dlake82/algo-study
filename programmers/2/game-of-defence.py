0cd47c7f40562bae9e8e39612ff0500257fab524
dlake82 <dlake0828@gmail.com>
2023-08-24 00:21:17 +0900

    game-of-defence

--- programmers/2/game-of-defence.py
+++ programmers/2/game-of-defence.py
@@ -2,24 +2,6 @@ from heapq import heappush, heappop
 
 
 def solution(n, k, enemy):
-    if k == len(enemy):
-        return k
-
-    answer, now, h = 0, 0, []
-
-    for i, e in enumerate(enemy):
-        now += e
-        heappush(h, -e)
-        if n < now:
-            if k == 0:
-                return i
-            k -= 1
-            now += heappop(h)
-
-    return answer
-
-
-def solution(n, k, enemy):
     hq = []
     for round, monster in enumerate(enemy):
         heappush(hq, monster)
