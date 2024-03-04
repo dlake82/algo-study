c8f0dca89bdc5ece663c529de4003a29e122fdf1
dlake82 <dlake0828@gmail.com>
2024-03-05 08:33:14 +0900

    feat: sort 하는 중

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# 기초
 def _sum(n):
     return n + (n + 1) // 2
 
@@ -29,6 +30,7 @@ def _min(arr):
 print(_min(arr))
 
 
+# 소수
 def is_prime(n):
     for i in range(2, int(n**0.5) + 1):
         if n % 2 == 0:
@@ -57,6 +59,7 @@ def erathosthenes(n):
 print(erathosthenes(20))
 
 
+# 유클리드 호제법
 def gcd(a, b):
     while b > 1:
         a, b = b, a % b
@@ -66,6 +69,7 @@ def gcd(a, b):
 print(gcd(123, 12))
 
 
+# 기하
 def sum_of_three_bar(a, b, c):
     if a + b > c:
         c = a + b - 1
@@ -99,8 +103,7 @@ print(turret([1, 1, 1, 1, 1, 5]))
 
 
 def house_of_bee(m):
-    a = 1
-    n = 1
+    a = n = 1
     if m == 1:
         return 1
     while a < m:
@@ -111,3 +114,59 @@ def house_of_bee(m):
 
 
 print(house_of_bee(8))
+
+
+# 자릿수의 합
+def d(n):
+    return sum(list(map(int, [s for s in str(n)])))
+
+
+def self_number(n):
+    arr = [True] * (n + 1)
+    for i in range(1, n + 1):
+        idx = i + d(i)
+        if idx < n + 1:
+            arr[idx] = False
+    return arr
+
+
+arr = self_number(20)
+for i, v in enumerate(arr):
+    if v:
+        print(i)
+
+
+def surprise_n(n):
+    answer = 0
+    for i in range(1, n + 1):
+        if i % d(i) == 0:
+            answer += 1
+    return answer
+
+
+print(surprise_n(1000))
+
+
+def count_digit(a, b, c):
+    n = a * b * c
+    arr = [0] * 10
+    ds = [s for s in str(n)]
+    for d in ds:
+        arr[int(d)] += 1
+    return arr
+
+
+for i in count_digit(150, 266, 427):
+    print(i)
+
+
+def b_sort(arr):
+    l = len(arr)
+    for i in range(l - 1):
+        for j in range(i + 1, l):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
+
+
+print(b_sort([1, 5, 4, 3, 2]))
